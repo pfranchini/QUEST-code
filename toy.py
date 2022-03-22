8213b2278a5e4be5034710b2eb1edac5be3023be
Paolo Franchini <pfranchi@linappserv3.pp.rhul.ac.uk>
2022-03-22 16:19:36 +0000

    Toy script final plot

--- toy.py
+++ toy.py
@@ -27,7 +27,7 @@ exec(open("mod_helium3.py").read())
 
 ## Input #####################################################
 
-energy = 1000 # [eV]
+energy = 10000 # [eV]
 
 
 ## Parameters ################################################
@@ -38,7 +38,7 @@ density = 6.05e3;  # [kg/m^3] Niobium-Titanium (NbTi)
 #=============================================================
 
 pressure = 10    # [bar] pressure
-t_base = 150e-6  # [K] base temperature
+t_base = 50e-6  # [K] base temperature
 d = 200e-9;      # [m] vibrating wire diameter
 
 t_b = 5.0   # [s] decay constant
@@ -50,8 +50,8 @@ v_rms = 15*1e-9  # [V] Error on voltage measurement
 
 #=============================================================
 
-N = 1000  # number of toys
-verbose=False # verbosity for plotting
+N = 100  # number of toys
+verbose=True # verbosity for plotting
 
 ## More routines: ###########################################
 
@@ -77,7 +77,8 @@ def DeltaWidth_from_Energy(E,PressureBar):
     DQ = np.array([])  # delta energy [eV]
     DW = np.array([])  # delta width [Hz]    
     
-    for dw in np.arange(0,2.5,0.001):  # Delta(Deltaf)
+    #for dw in np.arange(0,2.5,0.001):  # Delta(Deltaf)
+    for dw in np.arange(0,2.5,0.01):  # Delta(Deltaf)  FASTER
         T2= Temperature_from_Width(W0+dw,PressureBar)
         T1= Temperature_from_Width(W0,PressureBar)
         DQ = np.append(DQ,(heat_capacity_Cv_B_phase_intergral_from_0(T2, PressureBar) - heat_capacity_Cv_B_phase_intergral_from_0(T1, PressureBar)) * volume * 6.242e+18) # [eV]
@@ -102,8 +103,8 @@ def DeltaWidth_from_Energy(E,PressureBar):
     return deltawidth
 
 # Define signal fit function Dw vs time
-def df(t,fb,d):
-    return fb + np.heaviside(t-5,1)*(d*np.power(t_b/t_w,t_w/(t_b-t_w))*(t_b/(t_b - t_w))*(np.exp(-(t-5)/t_b) - np.exp(-(t-5)/t_w)))
+def df(_t,_fb,_d):
+    return _fb + np.heaviside(_t-5,1)*(_d*np.power(t_b/t_w,t_w/(t_b-t_w))*(t_b/(t_b - t_w))*(np.exp(-(_t-5)/t_b) - np.exp(-(_t-5)/t_w)))
 
 ###########################################################
 
@@ -173,8 +174,23 @@ if __name__ == "__main__":
     error = np.array([])
     e = np.array([])
     
-    #    for energy in np.arange(10000, 100e3, 2e3):
-    for energy in np.arange(100, 3000, 200):
+    for energy in np.arange(100, 900, 50):
+
+        mu, sigma = Toy(energy)
+        print(mu,sigma,sigma/mu*100,"%")
+
+        error = np.append(error,sigma/mu*100)
+        e = np.append(e,energy)
+
+    for energy in np.arange(1000, 9000, 500):
+
+        mu, sigma = Toy(energy)
+        print(mu,sigma,sigma/mu*100,"%")
+
+        error = np.append(error,sigma/mu*100)
+        e = np.append(e,energy)
+
+    for energy in np.arange(10000, 100000, 2000):
 
         mu, sigma = Toy(energy)
         print(mu,sigma,sigma/mu*100,"%")
@@ -183,9 +199,11 @@ if __name__ == "__main__":
         e = np.append(e,energy)
 
     plt.plot(e/1000,error, linestyle='', marker='o', color="black")
+    plt.xscale('log')
     plt.xlabel('Energy [KeV]')
     plt.ylabel('Error [%]')
     plt.savefig('error.pdf')
+    plt.savefig('error.png')
     plt.show()
 
     
