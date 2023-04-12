Fixed bandwidth calculation for the toys.

toys/config.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 ##############################################################
 ##
 ##  List of parameters common to all the toys
@@ -9,29 +11,32 @@ import numpy as np
 
 ## Cell:  ####################################################
 
-volume = 1e-6      # [m^3] Helium-3 cell
+#volume = 1e-6     # [m^3] Helium-3 cell
+volume = 0.315e-6  # [m^3] # Bolometer boxes with 0.5mm hole in 100um PET wall. Estimated time constant 1.0s (https://github.com/slazav/cryo_data/blob/main/f4_data.md)
 
 
 ## Cryostat:  ################################################
 
-pressure = 0     # [bar] pressure
-ttc= 0.1         # T/Tc
-energy = 10;     # [eV] deposited energy
+pressure = 0       # [bar] pressure
+ttc = 0.13         # T/Tc
+energy = 1000;     # [eV] deposited energy
 
 
 ## Wire:  ####################################################
 
+#diameter = 4.5e-6;  # [m] vibrating wire diameter
 diameter = 400e-9;  # [m] vibrating wire diameter
-l = 2e-3            # [m] vibrating wire lenght 
+l = 2e-3            # [m] vibrating wire length
 density = 6.05e3;   # [kg/m^3] Niobium-Titanium (NbTi)
 
 
 ## Lock-in parameters:  ######################################
 
-t_b = 5.00  # [s] decay constant
-amp=100     # gain of the voltage cold amplifier
+t_b = 5.00   # [s] decay constant
+amp = 100    # gain of the voltage cold amplifier
 v_h = amp*np.pi/2*1e-7  # [V] Base voltage height for a v=1mm/s
-v_rms = 7.9*1e-9        # [V] Error on voltage measurement for a lock-in amplifier
+lockin_bandwidth = 10   # [Hz] Bandwidth of the lock-in amplifier
+v_rms = 7.9*1e-9        # [V] Error on voltage measurement for a lock-in amplifier coming from the datasheet, considering a bandwidth of 10 Hz
 
 
 ## SQUID parameters:  ########################################

toys/shot_toy.py
@@ -103,7 +103,8 @@ def DeltaWidth_from_Energy(E,PressureBar,BaseTemperature,Diameter):
 
 # Define the noise function for shot-noise
 def noise(_deltaf,_fb,_pressure,_temperature,_diameter):
-    bandwidth = np.pi*_fb/2 # Samuli docet
+    #bandwidth = np.pi*_fb/2 # Samuli docet
+    bandwidth = min(np.pi*_fb/2, lockin_bandwidth) # Min between lock-in bandwidth and SQUID bandwidth as in Lev's note
     gap = energy_gap_in_low_temp_limit(_pressure)
     mass=mass_effective(_pressure)*atomic_mass # effective mass [kg]
     particle_density=1/(np.pi**2)*np.power(2*mass/Plankbar_const**2,3/2)*np.sqrt(Fermi_momentum(_pressure)**2/(2*mass))*Boltzmann_const*_temperature*np.exp(-gap/(Boltzmann_const*_temperature)) # QP number density, eq.31
@@ -140,7 +141,7 @@ def Toy(_energy,_pressure,_temperature,_diameter):
     print("Base width:      ",f_base*1000, " mHz")
     print("Width variation: ",delta*1000,  " mHz")
     print("t_w: ",t_w, "s")
-    print("Bandwidth: ",np.pi*f_base/2, " Hz")
+    print("Bandwidth: ",min(np.pi*f_base/2, lockin_bandwidth), " Hz")
     
     t = np.linspace(4.5, 50, 1000) # time
 

toys/shot_toy_energy.py
@@ -93,7 +93,6 @@ def DeltaWidth_from_Energy(E,PressureBar,BaseTemperature):
         DW = np.append(DW,dw)
         
     # Draw the plot 
-    '''
     if verbose and E==10000: 
         plt.plot(DQ/1e3,DW*1e3,label='DQvsDW')
         plt.title('Width variation vs Deposited energy')
@@ -101,7 +100,6 @@ def DeltaWidth_from_Energy(E,PressureBar,BaseTemperature):
         plt.xlabel('$\Delta$Q [KeV]')
         plt.ylabel('$\Delta(\Delta f)$ [mHz]')
         plt.show()
-    '''
     
     # Fit line to extract the slope alpha: DQ = alpha * DW
     global alpha
@@ -117,7 +115,8 @@ def DeltaWidth_from_Energy(E,PressureBar,BaseTemperature):
 
 # Define the noise function for shot-noise
 def noise(_deltaf,_fb,_pressure,_temperature):
-    bandwidth = np.pi*_fb/2 # Samuli docet  
+    #bandwidth = np.pi*_fb/2 # Samuli docet
+    bandwidth = min(np.pi*_fb/2, lockin_bandwidth) # Samuli docet
     gap = energy_gap_in_low_temp_limit(_pressure)
     mass=mass_effective(_pressure)*atomic_mass # effective mass [kg]
     particle_density=1/(np.pi**2)*np.power(2*mass/Plankbar_const**2,3/2)*np.sqrt(Fermi_momentum(_pressure)**2/(2*mass))*Boltzmann_const*_temperature*np.exp(-gap/(Boltzmann_const*_temperature)) # QP number density, eq.31
@@ -149,8 +148,8 @@ def Toy(energy):
     
     print("Base width:      ",f_base*1000, " mHz")
     print("Width variation: ",delta*1000,  " mHz")
-    print("t_w: ",t_w, "s")
-    
+    print("t_w: ",t_w, "s") 
+          
     #t = np.linspace(0, 50, 200) # time
     t = np.linspace(4.5, 50, 1000) # time
 
@@ -166,7 +165,7 @@ def Toy(energy):
         # Add noise based on QP shot noise
         for j in range(len(deltaf)):
             deltaf[j] = np.random.normal(deltaf[j],noise(deltaf[j],f_base,pressure,t_base), 1)
-
+            
         # Random noise    
         #noise = np.random.normal(0, 1e-3, len(t))
         #deltaf = deltaf + noise
@@ -320,7 +319,7 @@ if __name__ == "__main__":
     error = np.array([])
     e = np.array([])
 
-    Run_Toy(1e-2, 0.9, 1e-2)
+    Run_Toy(1e-2, 0.9, 1e-1)
     Run_Toy(1, 9, 1)
     Run_Toy(10, 90, 10)
     Run_Toy(100, 900, 50)

toys/squid_toy.py
@@ -124,8 +124,9 @@ def Voltage_Error(_fb,_temperature,_diameter): # (base width, temperature, diame
     m = np.power(_diameter/2,2)*np.pi*density # kg/m
     Z=l*np.power(B,2)/(2*np.pi*m*_fb)
     v_rms = np.sqrt( (np.power(Z+R,2) + np.power(w0*L,2))*S*np.pi*_fb/2/np.power(M,2) + 4*Boltzmann_const*_temperature*R*np.pi*_fb/2 + Boltzmann_const*_temperature*l*np.power(B,2)/m ) # [V]
-    nep = 0.250 # Noise Equivalent Power in units of bandwidth
-    return v_rms*np.sqrt(nep)
+    #nep = 0.250 # Noise Equivalent Power in units of bandwidth
+    bandwidth = min(np.pi*_fb/2, lockin_bandwidth) # Min between lock-in bandwidth and SQUID bandwidth as in Lev's note 
+    return v_rms*np.sqrt(bandwidth)
 
 ###########################################################
 