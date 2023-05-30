a4b698763fd043fe3b27f9baf1d7538027bc96de
Paolo Franchini <pfranchini@gmail.com>
2023-05-30 16:07:29 +0100

    Script for the plots for the paper. Minor change on the note for t0 in W.

--- toys/plot_for_paper_shot.py
+++ toys/plot_for_paper_shot.py
@@ -3,7 +3,15 @@ Plots on the same plot the Error vs Pressure, Diameter, T/Tc for the
 lock-in amplfier and the SQUID readout toys produce before and saved
 in a text file.
 
+Two plots:
+  E | T/Tc
+  d | pressure
+
 Input: txt files produced by the toy scripts.
+Usage:  
+   cd output direcotry
+   python ../../plot_for_paper_shot.py
+
 '''
 
 import numpy as np
@@ -23,64 +31,76 @@ def heat_capacity(T,A):
 
 #============================================================
 
+### First plot:
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
 # Energy:
 
-with open('output/lockin-error.txt') as f:
+with open('lockin-error.txt') as f:
     lines = f.readlines()[1:]
     x1 = [float(line.split()[0]) for line in lines]
     y1 = [float(line.split()[1]) for line in lines]
 
-with open('output/squid_toy-error.txt') as f:
+with open('squid_toy-error.txt') as f:
     lines = f.readlines()[1:]
     x2 = [float(line.split()[0]) for line in lines]
     y2 = [float(line.split()[1]) for line in lines]
 
-with open('output/shot-error.txt') as f:
+with open('shot-error.txt') as f:
     lines = f.readlines()[1:]
     x3 = [float(line.split()[0]) for line in lines]
     y3 = [float(line.split()[1]) for line in lines]
 
-plt.plot(x1,y1,label='Lock-in amplifier',color='dodgerblue',linestyle='-')
-plt.plot(x2,y2,label='SQUID readout',color='red',linestyle='--')
-plt.plot(x3,y3,label='QP shot noise',color='green',linestyle=':')
-plt.xlabel('Energy [eV]')
-plt.ylabel('Error [%]')
-plt.xscale('log')
-plt.yscale('log')
-plt.legend()
-plt.savefig("Error_comparison.pdf")
-plt.savefig("Error_comparison.png")
-plt.show()
+ax1.text(0.05, 0.90, 'a', transform=ax1.transAxes, fontsize=16, verticalalignment='top', horizontalalignment='left', weight='bold')
+ax1.plot(x1,y1,label='Lock-in amplifier',color='dodgerblue',linestyle='-')
+ax1.plot(x2,y2,label='SQUID readout',color='red',linestyle='--')
+ax1.plot(x3,y3,label='QP shot noise',color='green',linestyle=':')
+ax1.set_xlabel('Energy [eV]')
+ax1.set_ylabel('Error [%]')
+ax1.set_xscale('log')
+ax1.set_yscale('log')
+ax1.legend()
 
-# Pressure:
+# T/Tc:
 
-with open('output/lockin_toy-error-pressure.txt') as f:
+with open('lockin_toy-error-temperature.txt') as f:
     lines = f.readlines()[1:] #skip first line
     x1 = [float(line.split()[0]) for line in lines]
     y1 = [float(line.split()[1]) for line in lines]
 
-with open('output/squid_toy-error-pressure.txt') as f:
+popt, pcov = curve_fit(heat_capacity,x1,y1)
+A = popt
+
+with open('squid_toy-error-temperature.txt') as f:
     lines = f.readlines()[1:]
     x2 = [float(line.split()[0]) for line in lines]
     y2 = [float(line.split()[1]) for line in lines]
 
-with open('output/shot_toy-error-pressure.txt') as f:
+with open('shot_toy-error-temperature.txt') as f:
     lines = f.readlines()[1:]
     x3 = [float(line.split()[0]) for line in lines]
     y3 = [float(line.split()[1]) for line in lines]
 
-plt.plot(x1,y1,label='Lock-in amplifier',color='dodgerblue',linestyle='-')
-plt.plot(x2,y2,label='SQUID readout',color='red',linestyle='--')
-plt.plot(x3,y3,label='QP shot noise',color='green',linestyle=':')
-plt.xlabel('Pressure [bar]')
-plt.ylabel('Error [%]')
-#plt.xscale('log')
-plt.yscale('log')
-#plt.legend()
-plt.savefig("Error_pressure-comparison.pdf")
-plt.savefig("Error_pressure-comparison.png")
+ax2.text(0.05, 0.90, 'b', transform=ax2.transAxes, fontsize=16, verticalalignment='top', horizontalalignment='left', weight='bold')
+ax2.plot(x1,y1,label='Lock-in amplifier',color='dodgerblue',linestyle='-')
+ax2.plot(x2,y2,label='SQUID readout',color='red',linestyle='--')
+ax2.plot(x3,y3,label='QP shot noise',color='green',linestyle=':')    
+ax2.plot(x1, heat_capacity(np.array(x1), A),linestyle=':', marker='',color="black")
+ax2.set_xlabel('T/T$_c$')
+ax2.set_ylabel('Error [%]')
+#ax2.xscale('log')
+ax2.set_yscale('log')
+#ax2.legend()
+
+plt.tight_layout()
+plt.subplots_adjust(wspace=0.3)
+plt.savefig("Error_energy-temperature_comparison.pdf")
+plt.savefig("Error_energy-temperature_comparison.png")
 plt.show()
+
+
+### Second plot:
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
 # Diameter:
 
@@ -91,66 +111,62 @@ y1=np.array([])
 y2=np.array([])
 y3=np.array([])
 
-with open('output/lockin_toy-error-diameter.txt') as f:
+with open('lockin_toy-error-diameter.txt') as f:
     lines = f.readlines()[1:] #skip first line
     for line in lines:
         x1 = np.append(x1, float(line.split()[0]))
         y1 = np.append(y1, float(line.split()[1]))
 
-with open('output/squid_toy-error-diameter.txt') as f:
+with open('squid_toy-error-diameter.txt') as f:
     lines = f.readlines()[1:]
     for line in lines:
         x2 = np.append(x2, float(line.split()[0]))
         y2 = np.append(y2, float(line.split()[1]))
 
-with open('output/shot_toy-error-diameter.txt') as f:
+with open('shot_toy-error-diameter.txt') as f:
     lines = f.readlines()[1:]
     for line in lines:
         x3 = np.append(x3, float(line.split()[0]))
         y3 = np.append(y3, float(line.split()[1]))
 
-plt.plot(x1*1e9,y1,label='Lock-in amplifier',color='dodgerblue',linestyle='-')
-plt.plot(x2*1e9,y2,label='SQUID readout',color='red',linestyle='--')
-plt.plot(x3*1e9,y3,label='QP shot noise',color='green',linestyle=':')
-plt.xlabel('Diameter [nm]')
-plt.ylabel('Error [%]')
-plt.yscale('log')
-#plt.legend()
-plt.savefig("Error_diameter-comparison.pdf")
-plt.savefig("Error_diameter-comparison.png")
-plt.show()
+ax1.text(0.05, 0.90, 'a', transform=ax1.transAxes, fontsize=16, verticalalignment='top', horizontalalignment='left', weight='bold')
+ax1.plot(x1*1e9,y1,label='Lock-in amplifier',color='dodgerblue',linestyle='-')
+ax1.plot(x2*1e9,y2,label='SQUID readout',color='red',linestyle='--')
+ax1.plot(x3*1e9,y3,label='QP shot noise',color='green',linestyle=':')
+ax1.set_xlabel('Diameter [nm]')
+ax1.set_ylabel('Error [%]')
+ax1.set_yscale('log')
+ax1.legend()
 
-# T/Tc:
+# Pressure:
 
-with open('output/lockin_toy-error-temperature.txt') as f:
+with open('lockin_toy-error-pressure.txt') as f:
     lines = f.readlines()[1:] #skip first line
     x1 = [float(line.split()[0]) for line in lines]
     y1 = [float(line.split()[1]) for line in lines]
 
-popt, pcov = curve_fit(heat_capacity,x1,y1)
-A = popt
-
-with open('output/squid_toy-error-temperature.txt') as f:
+with open('squid_toy-error-pressure.txt') as f:
     lines = f.readlines()[1:]
     x2 = [float(line.split()[0]) for line in lines]
     y2 = [float(line.split()[1]) for line in lines]
 
-with open('output/shot_toy-error-temperature.txt') as f:
+with open('shot_toy-error-pressure.txt') as f:
     lines = f.readlines()[1:]
     x3 = [float(line.split()[0]) for line in lines]
     y3 = [float(line.split()[1]) for line in lines]
 
-plt.plot(x1,y1,label='Lock-in amplifier',color='dodgerblue',linestyle='-')
-plt.plot(x2,y2,label='SQUID readout',color='red',linestyle='--')
-plt.plot(x3,y3,label='QP shot noise',color='green',linestyle=':')    
-#plt.plot(x1, heat_capacity(np.array(x1), A)*5e9,linestyle=':', marker='',color="black")
-plt.xlabel('T/T$_c$')
-plt.ylabel('Error [%]')
-#plt.xscale('log')
-plt.yscale('log')
-#plt.legend()
-plt.savefig("Error_temperature-comparison.pdf")
-plt.savefig("Error_temperature-comparison.png")
+ax2.text(0.05, 0.90, 'b', transform=ax2.transAxes, fontsize=16, verticalalignment='top', horizontalalignment='left', weight='bold')
+ax2.plot(x1,y1,label='Lock-in amplifier',color='dodgerblue',linestyle='-')
+ax2.plot(x2,y2,label='SQUID readout',color='red',linestyle='--')
+ax2.plot(x3,y3,label='QP shot noise',color='green',linestyle=':')
+ax2.set_xlabel('Pressure [bar]')
+ax2.set_ylabel('Error [%]')
+#ax2.xscale('log')
+ax2.set_yscale('log')
+#ax2.legend()
+
+plt.tight_layout()
+plt.subplots_adjust(wspace=0.3)
+plt.savefig("Error_diameter-pressure_comparison.pdf")
+plt.savefig("Error_diameter-pressure_comparison.png")
 plt.show()
-
-
