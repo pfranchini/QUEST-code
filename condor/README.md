2dc013288a1015859fafb35f6e6c06eeb1cbced9
Paolo Franchini <franchinip@pplxint13.physics.ox.ac.uk>
2026-07-09 14:18:42 +0100

    Updated scripts for counting non-rethrow and rethrow simulations results.

--- condor/README.md
+++ condor/README.md
@@ -1,5 +1,7 @@
 
-Some scripts for the HTCondor batch system (RHUL/Lancaster)
+Some scripts for the HTCondor batch system (Oxford/RHUL, mostly used now in Oxford)
+
+`batch`: contains scripts used for Oxford ND3 simulation, both for non-rethrow and rethrow volumes.
 
 Usage:
 ```
--- condor/batch/count-a.sh
+++ condor/batch/count-a.sh
@@ -1,25 +1,45 @@
 dir=$1
 
+#=============================
+check_existing=true
+#=============================
+
 if [ ! -d "$dir" ]; then
    exit 1
 fi
  
 cd $dir/output-a
 
-echo "Counting and merging..."
+echo "Counting..."
 roots=`find . -type f -name "output*.root" -size +1k -exec ls -lh {} \; | wc -l`
-jobs=`grep -R "End of the session" * | wc -l`
+jobs=$(find . -type f -name '*.out' -exec grep -H "End of the session" {} + | wc -l)
 
-hadd -v 0 -f -k -j 10 merge.root */*.root
-events=`grep -h "Number of events" 1/* | tail -n1 | awk {'print $4'}`
+merge=true
+if [ "$check_existing" = true ] ; then
+    if [ -f "merge.root" ]; then
+	filesize=$(stat -c%s "merge.root")
+	if [ "$filesize" -gt 10485 ]; then
+	    echo "merge.root is already present."
+	    ls -l merge.root
+	    merge=false
+	fi
+    fi
+fi
+	
+if [ "$merge" = true ] ; then
+    echo "Merge files..."
+    #hadd -v 0 -f -k -j 10 merge.root */*.root
+    hadd -v 0 -f -k -j 5 merge.root */*.root
+fi
+
+#hadd -v 0 -f -k merge.root 1/*.root  # otherwise the output is too big
+events=`grep -h "Number of events" 1/*.out | tail -n1 | awk {'print $4'}`
 
 echo
 echo "Root files completed: " $roots
 echo "Jobs completed:       " $jobs
 echo "Simulated events/job: " $events
 
-root -q ~/dataQUEST/QUEST/ND3/batch/code-rethrow/code-a/plot-rethrow.c
-
-
+#root -q ~/dataQUEST/QUEST/ND3/batch/code-rethrow/code-a/plot-rethrow.c
 
 cd -
